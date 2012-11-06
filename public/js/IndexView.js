71c0d52f858d6fe4dd8d92b332aacc288cc657a1
Ben Robbins <ben.robbins@bazaarvoice.com>
2012-11-06 15:52:51 -0600

    added some fake json data

--- public/js/IndexView.js
+++ public/js/IndexView.js
@@ -1,10 +1,19 @@
 define([
   'ember',
-  'hbs!template/index'
+  'LoadData',
+  'hbs!template/index',
+  'hbs!template/listItem'
 ],
-function(Ember, indexTemplate) {
+function(Ember, LoadData, indexTemplate, listItemTemplate) {
   var IndexView = Ember.View.extend({
-    template: indexTemplate
+    template: indexTemplate,
+    listView: Ember.CollectionView.extend(LoadData, {
+      dataUrl: '/json/data.json',
+      dataKey: 'data',
+      itemViewClass: Ember.View.extend({
+        template: listItemTemplate
+      })
+    })
   });
   return IndexView;
 });
